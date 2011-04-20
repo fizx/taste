9b28d9ac855cf6aa7daaea750692098c399cce8d
Kyle Maxwell <kyle@kylemaxwell.com>
2011-04-20 08:45:25 -0700

    misc

--- project/build/TasteProject.scala
+++ project/build/TasteProject.scala
@@ -1,13 +1,14 @@
 import sbt._
 
 class TasteProject(info: ProjectInfo) extends DefaultProject(info) {
-  val slf4jVersion      = "1.6.0"
+  val slf4jVersion      = "1.6.1"
   val luceneVersion     = "3.1.0"
   val scalaTestVersion  = "1.3"
   val ioVersion         = "2.0.1"
   val utilVersion       = "1.8.9"
   
   val io = "commons-io" % "commons-io" % ioVersion
+  val codec = "commons-codec" % "commons-codec" % "1.5"
   
   val thrift = "thrift" % "libthrift" % "0.5.0"
   
@@ -19,11 +20,14 @@ class TasteProject(info: ProjectInfo) extends DefaultProject(info) {
   val luceneQ = "org.apache.lucene" % "lucene-queryparser" % luceneVersion
     
   val sfl4japi = "org.slf4j" % "slf4j-api" % slf4jVersion
-  val sfl4jnop = "org.slf4j" % "slf4j-nop" % slf4jVersion
+  val sfl4jlog4j = "org.slf4j" % "slf4j-log4j12" % slf4jVersion
+  val log4j = "log4j" % "log4j" % "1.2.9"
   
   val scalaTest = "org.scalatest" % "scalatest" % "1.3" % "test"
   val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test"
-  
+
+  // val repo1 = "repo1" at 
+  //     "http://repo1.maven.org/maven2/"
   val twitter = "Twitter" at
       "http://maven.twttr.com"
   val scalaToolsSnapshots = "Scala Tools Repository" at
@@ -34,6 +38,8 @@ class TasteProject(info: ProjectInfo) extends DefaultProject(info) {
       "https://oss.sonatype.org/content/repositories/releases"
   val fuseSourceSnapshots = "FuseSource Snapshot Repository" at 
       "http://repo.fusesource.com/nexus/content/repositories/snapshots"
+  val clojars = "Clojars" at 
+      "http://clojars.org/repo/"
       
   def thriftSources = (mainSourcePath / "thrift" ##) ** "*.thrift"
   def thriftBin = "thrift"
